9129fd6c5c5c664a6059e526428018d1dd811db9
Nikolaos  Goulios <ngoulios@c3r1p3.hive.fi>
2024-10-23 16:36:46 +0300

    functions for fractol

---
+++ string/ft_atof.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atof.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ngoulios <ngoulios@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/23 15:36:02 by ngoulios          #+#    #+#             */
+/*   Updated: 2024/10/23 15:41:33 by ngoulios         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+double	ft_atof(const char *str)
+{
+	double results;
+	double fraction;
+	int sign;
+	int i;
+
+	results = 0.0;
+	fraction = 1.0;
+	sign = 1;
+	i = 0;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] && str[i] >= '0' && str[i] <= '9')
+	{
+		results = results *10.0 + (str[i] - '0');
+		i++;
+	}
+	if (str[i] == '.')
+	{
+		i++;
+		while (str[i] && str[i] >= '0' && str[i] <= '9')
+		{
+			fraction /= 10.0;
+			results += (str[i] - '0') * fraction;
+			i++;
+		}
+	}
+	return (results * sign);
+}
